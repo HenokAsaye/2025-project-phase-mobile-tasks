e82a67d7e6696fefa9402adacd91617463a37d11
Henok Asaye <henokasaye77@gmail.com>
2025-08-01 22:55:02 +0300

    done teh navigation issue there

--- mobile/HenokAsaye/task_6/lib/main.dart
+++ mobile/HenokAsaye/task_6/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:task_6/screens/add_product_screen.dart';
+import 'package:task_6/screens/detail_page.dart';
+import 'package:task_6/screens/search_product.dart';
+import './screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,15 +10,42 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/search': (context) => const ProductSearchPage(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/detail') {
+          final args = settings.arguments as Map<String, dynamic>;
+          return PageRouteBuilder(
+            pageBuilder: (_, __, ___) => DetailPage(product: args),
+            transitionsBuilder: (_, animation, __, child) {
+              const begin = Offset(1.0, 0.0);
+              const end = Offset.zero;
+              var tween = Tween(begin: begin, end: end)
+                  .chain(CurveTween(curve: Curves.easeInOut));
+              return SlideTransition(position: animation.drive(tween), child: child);
+            },
+          );
+        }
+        if (settings.name == '/add') {
+            final args = settings.arguments as Map<String, dynamic>?;
+            return MaterialPageRoute(
+              builder: (_) => AddProductPage(product: args),
+            );
+        }
+        return null;
+      },
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: HomePage(),
     );
   }
 }
