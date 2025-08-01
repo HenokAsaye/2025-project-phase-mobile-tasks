43244ca263c399ae8fed3c545506c7abf63f4ae8
Henok Asaye <henokasaye77@gmail.com>
2025-08-02 01:45:06 +0300

    modify the product model

--- mobile/HenokAsaye/task_6/lib/features/product/data/models/product_model.dart
+++ mobile/HenokAsaye/task_6/lib/features/product/data/models/product_model.dart
@@ -1,7 +1,6 @@
 import '../../domain/entities/product.dart';
 
-
-class ProductModel extends Product{
+class ProductModel extends Product {
   ProductModel({
     required super.id,
     required super.name,
@@ -10,17 +9,39 @@ class ProductModel extends Product{
     required super.price,
   });
 
-
-  factory ProductModel.fromJson(Map<String, dynamic> json){
-    return ProductModel(id: json['id'], name: json['name'], description: json['description'], imageUrl: json['imageUrl'], price: json['price']);
+  /// JSON -> Model
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      imageUrl: json['imageUrl'],
+      price: json['price'],
+    );
   }
-
-
-  Map<String ,dynamic> toJson() =>{
-    'id':id,
-    'name':name,
-    'description':description,
-    'imageUrl':imageUrl,
-    'price':price,
-  };
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'description': description,
+        'imageUrl': imageUrl,
+        'price': price,
+      };
+  Product toEntity() {
+    return Product(
+      id: id,
+      name: name,
+      description: description,
+      imageUrl: imageUrl,
+      price: price,
+    );
+  }
+  factory ProductModel.fromEntity(Product product) {
+    return ProductModel(
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      imageUrl: product.imageUrl,
+      price: product.price,
+    );
+  }
 }
