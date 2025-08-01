7ebd612b71b5c81b7e50ce67f3a63120c060ca63
Henok Asaye <henokasaye77@gmail.com>
2025-08-02 01:45:51 +0300

    Done task 11

--- mobile/HenokAsaye/task_6/lib/features/product/domain/repositories/product_repo_impl.dart
+++ mobile/HenokAsaye/task_6/lib/features/product/domain/repositories/product_repo_impl.dart
@@ -1,37 +1,56 @@
-import '../entities/Product.dart';
-import 'product_repository.dart';
+import 'package:task_6/features/product/data/datasources/product_local_datasource.dart';
+import 'package:task_6/features/product/data/datasources/product_remote_datasource.dart';
+import 'package:task_6/features/product/data/models/product_model.dart';
+import '../../domain/entities/product.dart';
+import '../../domain/repositories/product_repository.dart';
 
-class ProductRepositoryImpl implements ProductRepository{
-  final List<Product> _products = [];
+class ProductRepositoryImpl implements ProductRepository {
+  final ProductRemoteDataSource remoteDataSource;
+  final ProductLocalDataSource localDataSource;
 
-  @override
-  Future<void> createProduct(Product product) async{
-    _products.add(product);
-  }
-
-
-  @override
-  Future<List<Product>> getAllProduct() async{
-    return _products;
-  }
-
+  ProductRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
 
   @override
-  Future<Product> getProductById(String id) async{
-    return _products.firstWhere((p) => p.id == id);
+  Future<void> createProduct(Product product) async {
+    final productModel = ProductModel.fromEntity(product);
+    await remoteDataSource.createProduct(productModel);
+    final allProducts = await remoteDataSource.fetchAllProducts();
+    await localDataSource.cacheProducts(allProducts);
   }
 
+  @override
+  Future<List<Product>> getAllProduct() async {
+    try {
+      final remoteProducts = await remoteDataSource.fetchAllProducts();
+      await localDataSource.cacheProducts(remoteProducts);
+      return remoteProducts.map((model) => model.toEntity()).toList();
+    } catch (e) {
+      final localProducts = await localDataSource.getCachedProducts();
+      return localProducts.map((model) => model.toEntity()).toList();
+    }
+  }
+
+  @override
+  Future<Product> getProductById(String id) async {
+    final model = await remoteDataSource.fetchProductById(id);
+    return model.toEntity();
+  }
+
+  @override
+  Future<void> updateProduct(Product product) async {
+    final model = ProductModel.fromEntity(product);
+    await remoteDataSource.updateProduct(model);
+    final allProducts = await remoteDataSource.fetchAllProducts();
+    await localDataSource.cacheProducts(allProducts);
+  }
 
   @override
   Future<void> deleteProduct(String id) async {
-    return _products.removeWhere((p) => p.id == id);
-  }
-
-  @override
-  Future<void>updateProduct(Product product) async{
-    final index = _products.indexWhere((p) => p.id == product.id);
-    if(index !=-1){
-      _products[index] = product;
-    }
+    await remoteDataSource.deleteProduct(id);
+    final updated = await remoteDataSource.fetchAllProducts();
+    await localDataSource.cacheProducts(updated);
   }
 }
