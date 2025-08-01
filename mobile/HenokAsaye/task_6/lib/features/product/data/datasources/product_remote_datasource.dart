0e1ed1254e8cab4f64e95c0d3f32170e722b4f1c
Henok Asaye <henokasaye77@gmail.com>
2025-08-02 01:47:16 +0300

    Done task 11

---
+++ mobile/HenokAsaye/task_6/lib/features/product/data/datasources/product_remote_datasource.dart
@@ -0,0 +1,8 @@
+import 'package:task_6/features/product/data/models/product_model.dart';
+abstract class ProductRemoteDataSource {
+  Future<List<ProductModel>> fetchAllProducts();
+  Future<ProductModel> fetchProductById(String id);
+  Future<void> createProduct(ProductModel product);
+  Future<void> updateProduct(ProductModel product);
+  Future<void> deleteProduct(String id);
+}
