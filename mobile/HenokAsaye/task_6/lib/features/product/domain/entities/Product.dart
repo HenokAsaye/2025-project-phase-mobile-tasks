fba05962182fc80dfa324f03f08fca45c6ffa006
Henok Asaye <henokasaye77@gmail.com>
2025-08-03 00:05:50 +0300

    donetask14

--- mobile/HenokAsaye/task_6/lib/features/product/domain/entities/Product.dart
+++ mobile/HenokAsaye/task_6/lib/features/product/domain/entities/Product.dart
@@ -1,4 +1,4 @@
-class Product{
+class Product {
   final String id;
   final String name;
   final String description;
@@ -10,6 +10,25 @@ class Product{
     required this.name,
     required this.description,
     required this.imageUrl,
-    required this.price
+    required this.price,
   });
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Product &&
+          runtimeType == other.runtimeType &&
+          id == other.id &&
+          name == other.name &&
+          description == other.description &&
+          imageUrl == other.imageUrl &&
+          price == other.price;
+
+  @override
+  int get hashCode =>
+      id.hashCode ^
+      name.hashCode ^
+      description.hashCode ^
+      imageUrl.hashCode ^
+      price.hashCode;
 }
