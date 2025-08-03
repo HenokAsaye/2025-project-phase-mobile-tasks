8c94006d609762925eab7bb98b104bc666d07765
Henok Asaye <henokasaye77@gmail.com>
2025-08-03 11:49:43 +0300

    Done Task17

--- mobile/HenokAsaye/task_6/lib/features/product/domain/repositories/product_repository.dart
+++ mobile/HenokAsaye/task_6/lib/features/product/domain/repositories/product_repository.dart
@@ -1,9 +1,11 @@
+import 'package:dartz/dartz.dart';
 import '../entities/product.dart';
+import '../../../../core/error/failures.dart';
 
 abstract class ProductRepository {
-  Future<List<Product>> getAllProduct();
-  Future<Product> getProductById(String id);
-  Future<void> createProduct(Product product);
-  Future<void>updateProduct(Product product);
-  Future<void> deleteProduct(String id);
+  Future<Either<Failure, List<Product>>> getAllProduct();
+  Future<Either<Failure, Product>> getProduct(String id);
+  Future<Either<Failure, void>> createProduct(Product product);
+  Future<Either<Failure, void>> updateProduct(Product product);
+  Future<Either<Failure, void>> deleteProduct(String id);
 }
